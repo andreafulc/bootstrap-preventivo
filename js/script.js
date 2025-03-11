a37cf9cc497d16bd3c683737a7e8648b047a2c7c
fulciniti.andre@gmail.com <fulciniti.andre@gmail.com>
2025-03-11 11:12:10 +0100

    feat -  took all necessary element in the js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Take all the element
+
+const userForm = document.getElementById("ticketForm");
+console.log(userForm);
+
+const userNameInput = document.getElementById("userName");
+console.log(userNameInput);
+
+const userSurnameInput = document.getElementById("userSurname");
+console.log(userSurnameInput);
+
+const userMailInput = document.getElementById("mail");
+console.log(userMailInput);
+
+const userWorkTipeInput = document.getElementById("workType");
+console.log(userWorkTipeInput);
+
+const userInfoInput = document.getElementById("note");
+console.log(userInfoInput);
+
+const userPromoInput = document.getElementById("userPromo");
+console.log(userPromoInput);
+
